aa5710254502f00df2a1392eaebc68860e69ea8d
jingzong <jzhou227@ucr.edu>
2025-06-03 19:42:29 -0700

    update on 06/03/2025

--- sim_walking_main.py
+++ sim_walking_main.py
@@ -1,5 +1,5 @@
 import pybullet as p
-import time
+import json
 import numpy as np
 from helpers.pybullet_setup import pybullet_setup
 from helpers.robot_walk_sim import HexapodRobotWalk
@@ -45,7 +45,47 @@ if __name__ == "__main__":
         "j3_swing": round(np.deg2rad(10), 2),
     }
 
+    walking_seq = {
+        "joint_11": [], "joint_12": [], "joint_13": [],
+        "joint_41": [], "joint_42": [], "joint_43": [],
+        "joint_21": [], "joint_22": [], "joint_23": [],
+        "joint_51": [], "joint_52": [], "joint_53": [],
+        "joint_61": [], "joint_62": [], "joint_63": [],
+        "joint_31": [], "joint_32": [], "joint_33": []
+    }
+
     num_loops = 100  # Number of walking cycles
     for i in range(num_loops):
-        hexapod_walk.generate_walking_seq_fk(paras_walk)
-        hexapod_walk.walk(time_duration=0.1)
+        walking_seq_return = hexapod_walk.generate_walking_seq_fk(paras=paras_walk)
+        for leg_name in walking_seq.keys():
+            if leg_name == "FL":
+                walking_seq["joint_11"].extend(walking_seq_return[leg_name]['theta1'])
+                walking_seq["joint_12"].extend(walking_seq_return[leg_name]['theta2'])
+                walking_seq["joint_13"].extend(walking_seq_return[leg_name]['theta3'])
+            elif leg_name == "FR":
+                walking_seq["joint_41"].extend(walking_seq_return[leg_name]['theta1'])
+                walking_seq["joint_42"].extend(walking_seq_return[leg_name]['theta2'])
+                walking_seq["joint_43"].extend(walking_seq_return[leg_name]['theta3'])
+            elif leg_name == "ML":
+                walking_seq["joint_21"].extend(walking_seq_return[leg_name]['theta1'])
+                walking_seq["joint_22"].extend(walking_seq_return[leg_name]['theta2'])
+                walking_seq["joint_23"].extend(walking_seq_return[leg_name]['theta3'])
+            elif leg_name == "MR":
+                walking_seq["joint_51"].extend(walking_seq_return[leg_name]['theta1'])
+                walking_seq["joint_52"].extend(walking_seq_return[leg_name]['theta2'])
+                walking_seq["joint_53"].extend(walking_seq_return[leg_name]['theta3'])
+            elif leg_name == "BL":
+                walking_seq["joint_31"].extend(walking_seq_return[leg_name]['theta1'])
+                walking_seq["joint_32"].extend(walking_seq_return[leg_name]['theta2'])
+                walking_seq["joint_33"].extend(walking_seq_return[leg_name]['theta3'])
+            elif leg_name == "BR":
+                walking_seq["joint_61"].extend(walking_seq_return[leg_name]['theta1'])
+                walking_seq["joint_62"].extend(walking_seq_return[leg_name]['theta2'])
+                walking_seq["joint_63"].extend(walking_seq_return[leg_name]['theta3'])
+
+        hexapod_walk.walk(time_duration=0.08)
+    
+    # save the turning sequence to a JSON file
+    file_path="helpers/data_cache/walking_seq.json"
+    with open(file_path, 'w') as f:
+        json.dump(walking_seq, f, indent=4)
