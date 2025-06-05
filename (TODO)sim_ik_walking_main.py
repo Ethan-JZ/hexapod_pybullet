fd0e4a9e34a8c3c4dcebb2689e126fc2c495d3ed
jingzong <jzhou227@ucr.edu>
2025-06-05 16:05:45 -0700

    update

--- (TODO)sim_ik_walking_main.py
+++ (TODO)sim_ik_walking_main.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
     robot_id = pybullet_setup(urdf_path="hexapod_pkg/urdf/hexapod.urdf")
 
     # solve the ik based on tipple goals
+    tipple_goals = None
     robot_ik_model = RobotIKSolver(tipple_goals=tipple_goals, robot_id=robot_id)
     walking_ik_seq_return = robot_ik_model.moving_seq()
